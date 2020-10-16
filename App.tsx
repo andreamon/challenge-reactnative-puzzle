8a0a7e9a37d6e0e20d2dfb6a366da51989c77a56
Andrea Montaño <andrea14_06_2@hotmail.com>
2020-10-16 18:58:37 -0300

    started app

--- App.tsx
+++ App.tsx
@@ -1,58 +1,37 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import Home from './components/Home'
-// import {
-//   Header,
-//   LearnMoreLinks,
-//   Colors,
-//   DebugInstructions,
-//   ReloadInstructions,
-// } from 'react-native/Libraries/NewAppScreen';
+// import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// declare const global: {HermesInternal: null | {}};
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+const Stack = createStackNavigator();
+
+import Intro from './components/Intro';
+import SearchNavigator from './navigators/SearchNavigator';
 
 const App = () => {
   return (
-    <Home />
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#403F3E',
+          },
+          headerTitleStyle: {
+            fontFamily: 'monospace',
+          },
+          headerTitleAlign: 'center',
+          headerTintColor: '#fff',
+        }}>
+        <Stack.Screen
+          name="Welcome"
+          component={Intro}
+          options={{title: 'Rick & Morty'}}
+        />
+        <Stack.Screen name="Search" component={SearchNavigator} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
-// const styles = StyleSheet.create({
-//   scrollView: {
-//     backgroundColor: Colors.lighter,
-//   },
-//   engine: {
-//     position: 'absolute',
-//     right: 0,
-//   },
-//   body: {
-//     backgroundColor: Colors.white,
-//   },
-//   sectionContainer: {
-//     marginTop: 32,
-//     paddingHorizontal: 24,
-//   },
-//   sectionTitle: {
-//     fontSize: 24,
-//     fontWeight: '600',
-//     color: Colors.black,
-//   },
-//   sectionDescription: {
-//     marginTop: 8,
-//     fontSize: 18,
-//     fontWeight: '400',
-//     color: Colors.dark,
-//   },
-//   highlight: {
-//     fontWeight: '700',
-//   },
-//   footer: {
-//     color: Colors.dark,
-//     fontSize: 12,
-//     fontWeight: '600',
-//     padding: 4,
-//     paddingRight: 12,
-//     textAlign: 'right',
-//   },
-// });
 
 export default App;
--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-/**
- * @format
- */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'https://rickandmortyapi.com/graphql',
+  cache: new InMemoryCache(),
+});
+
+const AppWithApollo = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
+
+AppRegistry.registerComponent(appName, () => AppWithApollo);
